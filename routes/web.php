56f1370ee0e61d3d4772196201bbc7f54137640d
mehdiCH92 <66846176+mehdiCH92@users.noreply.github.com>
2021-08-31 14:22:08 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,16 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-//Route::get('/', function () {
-//    return view('home');
-//});
-
-//Route::get('/about', function () {
-   // return view('about');
-//});
-
-Route::view('/','home');
+Route::view('/','home')->name('home');
 Route::view('/about','about');
+Route::view('/serch','serch');
+Route::view('/singel','singel');
+Route::view('/blog','blog');
+Route::view('/contact','contact');
+Route::view('/pro1','pro1');
+Route::view('/pro2','pro2');
+Route::view('/pro3','pro3');
+Route::view('/pro4','pro4');
+Route::view('/pro5','pro5');
+Route::view('/pro6','pro6');
+Route::post('/search','SearchController@search');
