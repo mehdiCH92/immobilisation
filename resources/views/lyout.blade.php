4cad5621dd30b26c9582f6fb892576170289de3c
mehdiCH92 <66846176+mehdiCH92@users.noreply.github.com>
2021-09-02 13:17:22 +0200

    Update lyout.blade.php

--- resources/views/lyout.blade.php
+++ resources/views/lyout.blade.php
@@ -38,7 +38,7 @@
                     <ul class="main-menu">
                         <li><a href="{{ url('/') }}">Home</a></li>
                         <li><a href="{{ url('/about') }}">About</a></li>
-                        <li><a href="{{ url('/blog') }}">Blog</a></li>
+                        
                         <li><a href="{{ url('/contact') }}">Contact</a></li>
 
                         <li class="top-social">
